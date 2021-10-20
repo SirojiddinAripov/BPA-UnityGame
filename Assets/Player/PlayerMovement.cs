6deabcc2f39acbd06300483bfb7a7ca3c8d2328b
MattIce0308 <89921070+MattIce0308@users.noreply.github.com>
2021-10-19 20:16:06 -0400

    Added Player Animations

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     Vector2 moveDirection;
+    public Animator animator;
 
     // Update is called once per frame
     private void Update()
@@ -25,6 +26,10 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector2(moveX, moveY).normalized;
+
+        animator.SetFloat("Horizontal", moveX);
+        animator.SetFloat("Vertical", moveY);
+        animator.SetFloat("Speed", moveDirection.sqrMagnitude);
     }
 
     private void Move()
